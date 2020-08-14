a736435da9adacc30d38d36dfd032d6d8e4ae866
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-08-13 19:19:26 -0700

    added componentDidMount

--- src/GameList/ListPage.js
+++ src/GameList/ListPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import request from 'superagent'
+// import request from 'superagent'
 import List from './List.js'
+import { fetchGames } from '../API-Calls/games-api.js'
+import { Link } from 'react-router-dom'
 
 export default class ListPage extends Component {
     state = {
@@ -8,12 +10,13 @@ export default class ListPage extends Component {
     }
 
     componentDidMount = async () => {
-        const data = await request.get(`https://lab-08-backend.herokuapp.com/games`)
+        const data = await fetchGames()
+        // const data = await request.get(`https://lab-08-backend.herokuapp.com/games`)
 
-        const parsed = data.body;
+        // const parsed = data.body;
 
         this.setState({
-            emptyGamesArr: parsed
+            emptyGamesArr: data.body
         })
 
     }
@@ -21,6 +24,15 @@ export default class ListPage extends Component {
     render() {
         return (
             <div>
+            {
+                this.state.emptyGamesArr.map((game) => {
+                    return <Link className="game" to={`/detail/${game.id}`} key={`${game.id}-${game.name}`}>
+                        <p>Name: {game.name}</p>
+                        <p>Genre: {game.genre}</p>
+                        <p>Mature: {game.mature}</p>
+                    </Link>
+                })
+            }
                 <List data={this.state.emptyGamesArr} />
             </div>
         )
