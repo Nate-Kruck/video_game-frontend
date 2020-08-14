651f2467a3c36816c67bbb2de529a58bdb1e078b
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-08-13 17:00:16 -0700

    finished styling

--- src/GameList/Games.js
+++ src/GameList/Games.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 
+
+const Emoji = props => (
+    <span
+        className="emoji"
+        role="img"
+        aria-label={props.label ? props.label : ""}
+        aria-hidden={props.label ? "false" : "true"}
+        >
+        {props.symbol}
+    </span>
+)
 export default class Games extends Component {
     render() {
         const {
             data
         } = this.props
         return (
-            <li key={Math.random()*200} className="gameBlock">
-                <h1>{data.name}</h1>
+            <div className = "gameBlock">
+            <li key={Math.random()*200} className="productGrid">
+                <h3>{data.name}</h3>
                 <p><img className="image" src={data.image} alt={data.name} width="200px" /></p>
                 <p>Game Genre: {data.genre}</p>
-                <p>Price: {data.price}</p>
-                <p>Rating: {data.rating}</p>
+                <p>Price: ${data.price}</p>
+                < p > Rating: {
+                    data.rating
+                } < Emoji symbol = "⭐" /></p>
                 <p>Mature: {data.mature ? 'Yes' : 'No'}</p>
             </li>
+            </div>
+            
         )
     }
 }
--- src/GameList/List.js
+++ src/GameList/List.js
@@ -8,7 +8,7 @@ export default class List extends Component {
             data
         } = this.props
         return (
-            <ul>
+            <ul className="container">
                 {data.map(game => <Games data={game} key={Math.random()*200} />)}
             </ul>
         )
